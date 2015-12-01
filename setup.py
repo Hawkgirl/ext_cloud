43a27a6d7a01db0299f6bbcd4e6fe8886d9b5271
hawkgirl <hawkgirlgit@example.com>
2015-12-01 14:16:45 +0530

    Adding setup file for pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup, find_packages
+
+setup(name='ext_cloud',
+      version='2015.10.13',
+      description='Multi cloud library',
+      url='https://github.com/Hawkgirl/ext_cloud/',
+      author='Hawkgirl',
+      maintainer='Hawkgril',
+      maintainer_email='hawkgirlgit@gmail.com',
+      license='BSD',
+      packages=find_packages()
+)
+
