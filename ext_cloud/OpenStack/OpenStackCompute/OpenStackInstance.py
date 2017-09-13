14f80735c25b22fc3326c6763e6d10d06d17551d
Hawkgirl <Hawkgirlgit@gmail.com>
2017-09-13 10:47:30 +0000

    tenant name in metrics should not have @ and . chars

--- ext_cloud/OpenStack/OpenStackCompute/OpenStackInstance.py
+++ ext_cloud/OpenStack/OpenStackCompute/OpenStackInstance.py
@@ -304,7 +304,7 @@ class OpenStackInstancecls(OpenStackBaseCloudcls, BaseInstancecls):
         metrics = []
         import time
         from ext_cloud.BaseCloud.BaseResources.BaseMetrics import BaseMetricscls
-        metric_str = 'openstack.tenant.' + self.tenant_name + '.instance.' + self.id + '.' + self.name + '.'
+        metric_str = 'openstack.tenant.' + self.tenant_name.replace('@','_').replace('.','_') + '.instance.' + self.id + '.' + self.name + '.'
 
         results = self.cpu_usage(start_time, end_time, count)
         for result in results:
